7c1e335a5cd0d918863ba49079f813cd518b8d3e
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-09-16 17:37:48 -0400

    Initial commit

---
+++ src/SelectionSort/Main.java
@@ -0,0 +1,60 @@
+package SelectionSort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numberArray = {2, 7, 4, 1, 5, 3};
+        selectionSort(numberArray);
+        printSortedArray(numberArray);
+
+    }
+
+    public static void selectionSort(int[] numberArray) {
+        int numberLength = numberArray.length;
+        for (int boundaryIndex = 0; boundaryIndex < numberLength - 1; boundaryIndex++) {
+//            find the minimum number of the unsorted array
+            int minIdx = minimumIndex(numberArray, boundaryIndex);
+//            swap the minimum number with the boundary number
+            swap(numberArray, minIdx, boundaryIndex);
+
+        }
+
+    }
+
+    public static int minimumIndex(int[] numberArray, int boundaryIndex) {
+        int minIndex = boundaryIndex;
+        for (int unsortedIndex = boundaryIndex + 1; unsortedIndex < numberArray.length; unsortedIndex++) {
+            if (numberArray[minIndex] > numberArray[unsortedIndex]) {
+                minIndex = unsortedIndex;
+            }
+
+        }
+        return minIndex;
+    }
+
+    public static void swap(int[] array, int indexOne, int indexTwo) {
+        int temp = array[indexOne];
+        array[indexOne] = array[indexTwo];
+        array[indexTwo] = temp;
+
+
+    }
+
+    public static void printSortedArray(int[] numberArray) {
+        System.out.println("The sorted array would be: ");
+        for (int number : numberArray) {
+            System.out.print(number + " ");
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
