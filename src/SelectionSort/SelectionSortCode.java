7922adbae0f62421dae178d725435a100471501a
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-10-14 21:15:10 -0400

    commit

---
+++ src/SelectionSort/SelectionSortCode.java
@@ -0,0 +1,28 @@
+package SelectionSort;
+
+public class SelectionSortCode {
+    public static void main(String[] args) {
+        int[] numberArray = {2, 7, 4, 1, 5, 3};
+        selectionSort(numberArray);
+        for (int i = 0; i < numberArray.length; i++) {
+            System.out.print(numberArray[i] + " ");
+
+        }
+    }
+
+    public static void selectionSort(int[] data) {
+        for (int boundaryIndex = 0; boundaryIndex < (data.length - 1); boundaryIndex++) {
+            int minIndex = boundaryIndex;
+            int minNumber = data[boundaryIndex];
+            for (int unsortedIndex = boundaryIndex + 1; unsortedIndex < data.length; unsortedIndex++) {
+                if (data[unsortedIndex] < minNumber) {
+                    minNumber = data[unsortedIndex];
+                    minIndex = unsortedIndex;
+                }
+            }
+
+            data[minIndex] = data[boundaryIndex];
+            data[boundaryIndex] = minNumber;
+        }
+    }
+}
---
+++ src/SelectionSort/SelectionSortJava.java
@@ -0,0 +1,31 @@
+package SelectionSort;
+
+public class SelectionSortJava {
+    public static void main(String[] args) {
+        int[] numberArray = {2, 7, 4, 1, 5, 3};
+        selectionSort(numberArray);
+        for (int i = 0; i < numberArray.length; i++) {
+            System.out.print(numberArray[i] + " ");
+        }
+    }
+
+    public static void selectionSort(int[] data) {
+        for (int boundary = 0; boundary < data.length; boundary++) {
+            // Find the minimum number of the unsorted array
+            int minIndex = boundary;
+            for (int unsortedIndex = boundary; unsortedIndex < data.length; unsortedIndex++) {
+                // Find the index of the minimum number
+                if (data[unsortedIndex] < data[minIndex]) {
+                    // Swap the index of found minimum number with the index of assumed minimum number
+                    minIndex = unsortedIndex;
+                }
+            }
+
+            // Swap the found minimum number with boundary number
+            int temp;
+            temp = data[minIndex];
+            data[minIndex] = data[boundary];
+            data[boundary] = temp;
+        }
+    }
+}
