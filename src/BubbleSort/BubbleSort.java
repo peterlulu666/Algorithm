12676c0cf8b1d8c5a23dce2789654362e43db072
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-11-24 13:45:46 -0500

    commit

---
+++ src/BubbleSort/BubbleSort.java
@@ -0,0 +1,27 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    System.out.println("Swap the number " + arr[j] +" in index " + j + " with the number " + arr[j + 1] + " in index " + (j + 1));
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 9, 5, 4, 7, 8, 6, 10};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
